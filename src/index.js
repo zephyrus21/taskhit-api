5e63aa4a5a9ccc8e5029b34454f01589f4258d38
zephyrus21 <pandey.piyus@gmail.com>
2022-03-20 20:44:44 +0530

    added cors policy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectWithDb = require("./config/db");
 const { task } = require("./routes/task");
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 4000;
